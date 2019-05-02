8536b3fe515971f47509262703c65342a0b5d22c
Giang Nguyen <luckytintinnz@yahoo.com>
2019-05-02 17:40:47 +1200

    Playing with GraphQL

---
+++ server.js
@@ -0,0 +1,32 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const todos = [
+  { task: 'First task', completed: false },
+  { task: 'Second task', completed: true },
+];
+
+const typeDefs = gql`
+  type Todo {
+    task: String,
+    completed: Boolean
+  }
+
+  type Query {
+    getTodos: [Todo]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getTodos: () => todos,
+  }
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`Server listening on ${url}`);
+});
